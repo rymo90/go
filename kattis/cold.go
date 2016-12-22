dd674568410a382fa9af8bac4a31a891ddc59785
rymo90 <red01.yassin@gmail.com>
2016-12-22 04:28:39 +0100

    finish
    just finished this easy code
    that determent how many time a number is inter zero.

--- kattis/cold.go
+++ kattis/cold.go
@@ -3,13 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	counter := 0
 	var n int
 	var t int
-	fmt.scan(&n)
-	s := make([]int, n)
+	fmt.Scan(&n)
+	//
 	for i := 0; i < n; i++ {
 		fmt.Scan(&t)
+		if t < 0 {
+			counter += 1
+		} else {
 
+		}
 	}
+	fmt.Println(counter)
 
 }
