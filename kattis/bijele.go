edfefe4007eed916daaa86963ea736b69e8fb84e
rymo90 <red01.yassin@gmail.com>
2016-12-29 05:47:43 +0100

    kattis
    Finished working with bijele!
    great exercise  !!

--- kattis/bijele.go
+++ kattis/bijele.go
@@ -6,31 +6,22 @@ func chest(cet, get []int) []int {
 	resultat := make([]int, 0)
 	counter := 0
 	for i := 0; i < 6; i++ {
-		if cet[i] == get[i] {
-			counter = 0
-			resultat = append(resultat, counter)
-		} else {
-			for j := 0; j <= 10; j++ {
-				if cet[i] > get[i] {
-					get[i]++
-					counter++
-					resultat = append(resultat, counter)
-					break
-				} else if cet[i] < get[i] {
-					get[i]--
-					counter--
-					resultat = append(resultat, counter)
-					break
-				}
-
+		for j := 0; j <= 10; j++ {
+			if cet[i] > get[i] {
+				get[i]++
+				counter++
+			} else if cet[i] < get[i] {
+				get[i]++
+				counter++
+			} else {
+				resultat = append(resultat, counter)
+				counter = 0
+				break
 			}
 		}
-
 	}
 	return resultat
-
 }
-
 func main() {
 	var input int
 	cet := []int{1, 1, 2, 2, 2, 8}
--- kattis/oddities.go
+++ kattis/oddities.go
@@ -1,29 +1,30 @@
 package main
+
 // check if the numer is even or odd
 // working with kattis
 
 import (
-    "fmt"
-    "strconv"
+	"fmt"
+	"strconv"
 )
 
 func main() {
 	var num, x int
 	fmt.Scan(&num)
-  s:= make([]string, num)
+	s := make([]string, num)
 	for i := 0; i < num; i++ {
-    fmt.Scan(&x)
-	  if (x%2==0) {
-      d:= strconv.Itoa(x)
-      //s = append(s, d)
-      s = append(s, d + " is even" )
-	  }else {
-      d:= strconv.Itoa(x)
-      s = append( s, d + " is odd")
-	  }
-  }
-  for j := 0; j < len(s); j++ {
-    fmt.Println(s[j])
-  }
+		fmt.Scan(&x)
+		if x%2 == 0 {
+			d := strconv.Itoa(x)
+			//s = append(s, d)
+			s = append(s, d+" is even")
+		} else {
+			d := strconv.Itoa(x)
+			s = append(s, d+" is odd")
+		}
+	}
+	for j := 0; j < len(s); j++ {
+		fmt.Println(s[j])
+	}
 
 }
